82e3d54cb016b5cfc62fed3e2b460898d143bc1d
Alvaro Lopez <al7aro@gmail.com>
2023-03-08 14:42:00 +0100

    Testing_With_Hooke

--- src/main.c
+++ src/main.c
@@ -15,14 +15,20 @@ int	main(void)
 
 	t_axis2D	ax;
 	axis_create(&ax, (Rectangle){WIDTH/2, HEIGHT/2, WIDTH * 0.9, 500});
-	ax.title = "TEST";
-	ax.x_label = "TEST X";
-	ax.y_label = "TEST Y";
-	ax.x_range = (Vector3){-10, 0.01 , 10};
-	ax.y_range = (Vector3){-1.5, 0.01, 1.5};
+	ax.x_range = (Vector3){-5, 0.01, 5};
+	ax.y_range = (Vector3){-2, 0.01, 2};
+	ax.title = "Hooke's Law";
+	ax.x_label = "x / m";
+	ax.y_label = "t / s";
 	axis_add_plot(&ax, plot_create_F(ax.x_range, cos, SOLID, NULL, ORANGE));
 	axis_add_plot(&ax, plot_create_F(ax.x_range, wave_test, SOLID, NULL, PINK));
-	axis_add_plot(&ax, plot_create_F(ax.x_range, test, SCATTER, NULL, BLACK));
+	axis_add_plot(&ax, plot_create_F(ax.x_range, test, SCATTER, NULL, GREEN));
+
+	t_hooke h;
+	hooke_init(&h, 1000);
+	hooke_reset(&h, 2, 0.01, 5, 0.4, 0);
+	hooke_vel_data(&h);
+	axis_add_plot(&ax, plot_create_D(h.steps, h.data + 1, SOLID, NULL, BLACK));
 
 	while (!WindowShouldClose())
 	{
