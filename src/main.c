e159d49e38c910c57e86cd3d9a45177e9e66774a
Alvaro Lopez <al7aro@gmail.com>
2023-03-13 12:42:04 +0100

    Reestructure

--- src/main.c
+++ src/main.c
@@ -1,18 +1,98 @@
 #include "main.h"
 #include <stdio.h>
 
+/*-------------------------------------------------------*/
+/*       Local variables (LOCAL TO THIS MODULE)          */
+/*       (module variables ~~> class variables)          */
+/*-------------------------------------------------------*/
+t_axis2D	ax, ax1, ax2;
+t_plot		pl;
+t_hooke h;
+float	tf = 0.8 * 5;
+float	k_value = 2, m_value = 0.01;
+float	k_value_prev = 2, m_value_prev = 0.01;
+
+/*-------------------------------------------------------*/
+/*       Local functions (LOCAL TO THIS MODULE)          */
+/*-------------------------------------------------------*/
+double cos2(double x) { return (sin(x) * cos(2 * x * x) * cos(2 * x)); }
+void l(){system("leaks rayplot");}
 double cosT(double x) { return(sin (x - GetTime())); }
 
 void	UpdateDrawFrame(void)
 {
-		//Update data each time
-		br_func(pl.x_data, pl.size, cosT, pl.y_data);
-		BeginDrawing();
-		ClearBackground(WHITE);
-		DrawFPS(0, 0);
+		/* AXIS */
+	//--------------------------------------------------------------------------------------
+	// free(ax.plots[0].title);
+	// title = strdup(TextFormat("%.1f %.1f %.1f", h.k, h.m, hooke_get_period(h)));
+	// ax.plots[0].title = title;
 
-		axis_show(&ax);
-		EndDrawing();
+	/* INPUT TESTS */
+	if (IsKeyDown('W'))
+		ax.bounds.y -= 3;
+	if (IsKeyDown('A'))
+		ax.bounds.x -= 3;
+	if (IsKeyDown('S'))
+		ax.bounds.y += 3;
+	if (IsKeyDown('D'))
+		ax.bounds.x += 3;
+
+	BeginDrawing();
+	ClearBackground(WHITE);
+	DrawFPS(PANEL_WIDTH + WIDTH - 75, 0);
+	axis_show(&ax);
+	axis_show(&ax1);
+	axis_show(&ax2);
+	//--------------------------------------------------------------------------------------
+
+	/* RAYGUI */
+	//--------------------------------------------------------------------------------------
+	GuiPanel((Rectangle){0, 0, PANEL_WIDTH, HEIGHT}, "  Hook's Attributes");
+	DrawText("K", PANEL_WIDTH/2, 110, 20, WHITE);
+	k_value = GuiSlider((Rectangle){PANEL_WIDTH/2 - 50, 130, 100, 30}, "1", "10", k_value, 1, 10);
+	if (k_value != k_value_prev)
+	{
+		k_value_prev = k_value;
+		hooke_set_k(&h, k_value);
+		hooke_vel_data(&h);
+		hooke_E_data(&h);
+		hooke_Ep_data(&h);
+		hooke_Ec_data(&h);
+	}
+	DrawText("m", PANEL_WIDTH/2, 170, 20, WHITE);
+	m_value = GuiSlider((Rectangle){PANEL_WIDTH/2 - 50, 190, 100, 30}, "0.01", "0.5", m_value, 0.01, 0.5);
+	if (m_value != m_value_prev)
+	{
+		m_value_prev = m_value;
+		hooke_set_m(&h, m_value);
+	}
+	if (GuiButton((Rectangle){PANEL_WIDTH/2 - 25, 50, 50, 50}, "Reset"))
+	{
+		k_value = 2;
+		m_value = 0.01;
+		hooke_set_m(&h, m_value);
+		hooke_set_k(&h, k_value);
+	}
+	hooke_vel_data(&h);
+	hooke_E_data(&h);
+	hooke_Ep_data(&h);
+	hooke_Ec_data(&h);
+	//--------------------------------------------------------------------------------------
+	/* RECTANGLE ANIMATION */
+	//--------------------------------------------------------------------------------------  RECTANGLE ANIMATION
+	//TODO: IMPROVE AND AUTOMATE ANIMATIONS
+	// t+=GetFrameTime();
+	// if (t>=tf) t = GetFrameTime();
+	// DrawCircle(point.x, point.y, 5, RED);
+	// point = axis_map_to_screen(&ax, (Vector2){h.data[(int)(t/h.d_t)].x, h.data[(int)(t/h.d_t)].y});
+	// axis_show(&ax2);
+	// rect = axis_map_to_screen(&ax2, (Vector2){h.data[(int)(t/h.d_t)].y, 0});
+	// // l1 = axis_map_to_screen(ax2, 0, 1);
+	// // l2 = axis_map_to_screen(ax2, 0, -1);
+	// // DrawLine(l1.x, l1.y, l2.x, l2.y, WHITE);
+	// DrawRectangle(rect.x-25, rect.y-25, 50, 50, LIGHTGRAY);
+	//------------------------------------------------------------------------------------------
+	EndDrawing();
 }
 
 int	main(void)
@@ -25,9 +105,6 @@ int	main(void)
 	// Vector2	point = (Vector2){.x=0, .y=0};
 	// Vector2	rect = (Vector2){.x=PANEL_WIDTH + WIDTH/2, .y=HEIGHT/2};
 	// float	t = 0;
-	float	tf = 0.8 * 5;
-
-	t_axis2D	ax;
 	axis_create(&ax, (Rectangle){PANEL_WIDTH + WIDTH/4, HEIGHT/4, WIDTH * 0.5, 400});
 	ax.x_range = (Vector3){0, 0.01, tf};
 	ax.y_range = (Vector3){-2, 0.01, 2};
@@ -35,7 +112,6 @@ int	main(void)
 	ax.x_label = "t / s";
 	ax.y_label = "x / m";
 
-	t_axis2D	ax1;
 	axis_create(&ax1, (Rectangle){PANEL_WIDTH + WIDTH - WIDTH/4, HEIGHT/4, WIDTH * 0.5, 400});
 	ax1.x_range = (Vector3){0, 0.01, tf};
 	ax1.y_range = (Vector3){-2, 0.01, 2};
@@ -43,7 +119,6 @@ int	main(void)
 	ax1.x_label = "t / s";
 	ax1.y_label = "E / J";
 
-	t_axis2D	ax2;
 	axis_create(&ax2, (Rectangle){PANEL_WIDTH + WIDTH/4, HEIGHT - HEIGHT/4, WIDTH * 0.5, 400});
 	ax2.x_range = (Vector3){0, 0.01, tf};
 	ax2.y_range = (Vector3){-10, 0.01, 10};
@@ -57,7 +132,6 @@ int	main(void)
 	// ax2.y_range = (Vector3){.x=-0.3, .y = 0.01, .z=0.3};
 	// ax2.legend = false;
 
-	t_hooke h;
 	hooke_init(&h, 1000);
 	// k = 2 | m = 0.01 | v0 = 0
 	// hooke_reset(&h, 2, 0.01, 6, 0.4, 0); //T = 2pi/sqrt(k/m) = 0.444
@@ -95,87 +169,10 @@ int	main(void)
 
 	//RAYGUI
 	GuiLoadStyle("./styles/jungle/jungle.rgs");
-	float	k_value = 2, m_value = 0.01;
-	float	k_value_prev = 2, m_value_prev = 0.01;
 	while (!WindowShouldClose())
 	{
-		/* AXIS */
-		//--------------------------------------------------------------------------------------
-		// free(ax.plots[0].title);
-		// title = strdup(TextFormat("%.1f %.1f %.1f", h.k, h.m, hooke_get_period(h)));
-		// ax.plots[0].title = title;
-
-		/* INPUT TESTS */
-		if (IsKeyDown('W'))
-			ax.bounds.y -= 3;
-		if (IsKeyDown('A'))
-			ax.bounds.x -= 3;
-		if (IsKeyDown('S'))
-			ax.bounds.y += 3;
-		if (IsKeyDown('D'))
-			ax.bounds.x += 3;
-
-		BeginDrawing();
-		ClearBackground(WHITE);
-		DrawFPS(PANEL_WIDTH + WIDTH - 75, 0);
-		axis_show(&ax);
-		axis_show(&ax1);
-		axis_show(&ax2);
-		//--------------------------------------------------------------------------------------
-
-		/* RAYGUI */
-		//--------------------------------------------------------------------------------------
-		GuiPanel((Rectangle){0, 0, PANEL_WIDTH, HEIGHT}, "  Hook's Attributes");
-		DrawText("K", PANEL_WIDTH/2, 110, 20, WHITE);
-		k_value = GuiSlider((Rectangle){PANEL_WIDTH/2 - 50, 130, 100, 30}, "1", "10", k_value, 1, 10);
-		if (k_value != k_value_prev)
-		{
-			k_value_prev = k_value;
-			hooke_set_k(&h, k_value);
-			hooke_vel_data(&h);
-			hooke_E_data(&h);
-			hooke_Ep_data(&h);
-			hooke_Ec_data(&h);
-		}
-		DrawText("m", PANEL_WIDTH/2, 170, 20, WHITE);
-		m_value = GuiSlider((Rectangle){PANEL_WIDTH/2 - 50, 190, 100, 30}, "0.01", "0.5", m_value, 0.01, 0.5);
-		if (m_value != m_value_prev)
-		{
-			m_value_prev = m_value;
-			hooke_set_m(&h, m_value);
-
-		}
-		if (GuiButton((Rectangle){PANEL_WIDTH/2 - 25, 50, 50, 50}, "Reset"))
-		{
-			k_value = 2;
-			m_value = 0.01;
-			hooke_set_m(&h, m_value);
-			hooke_set_k(&h, k_value);
-		}
-		hooke_vel_data(&h);
-		hooke_E_data(&h);
-		hooke_Ep_data(&h);
-		hooke_Ec_data(&h);
-		//--------------------------------------------------------------------------------------
-
-		/* RECTANGLE ANIMATION */
-		//--------------------------------------------------------------------------------------  RECTANGLE ANIMATION
-		//TODO: IMPROVE AND AUTOMATE ANIMATIONS
-		// t+=GetFrameTime();
-		// if (t>=tf) t = GetFrameTime();
-		// DrawCircle(point.x, point.y, 5, RED);
-		// point = axis_map_to_screen(&ax, (Vector2){h.data[(int)(t/h.d_t)].x, h.data[(int)(t/h.d_t)].y});
-		// axis_show(&ax2);
-		// rect = axis_map_to_screen(&ax2, (Vector2){h.data[(int)(t/h.d_t)].y, 0});
-		// // l1 = axis_map_to_screen(ax2, 0, 1);
-		// // l2 = axis_map_to_screen(ax2, 0, -1);
-		// // DrawLine(l1.x, l1.y, l2.x, l2.y, WHITE);
-		// DrawRectangle(rect.x-25, rect.y-25, 50, 50, LIGHTGRAY);
-		//------------------------------------------------------------------------------------------
-		EndDrawing();
+		UpdateDrawFrame();
 	}
-	free(ax.plots[0].title);
 	hooke_destroy(&h);
-	//axis_destroy(&ax);
 	CloseWindow();
 }
